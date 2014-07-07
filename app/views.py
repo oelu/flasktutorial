6bc57d702691e6c74935744d33f1c2facdaa6b8e
oelu <olivierschenk@gmail.com>
2014-07-07 21:09:48 +0200

    added login form

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from flask import render_template
 from app import app
+from forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = {'nickname': 'Miguel'} # fake user
 
@@ -22,3 +22,14 @@ def index():
                            title = 'Home',
                            user = user,
                            posts = posts)
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form)
